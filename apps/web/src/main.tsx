d5b5488da8045a28e89c3f529b95daf9e4568b78
Twin Edo <twinedo.dev@gmail.com>
2025-07-09 22:30:05 +0700

    add import css

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 import React from "react";
+import './style.css'
 
 // Clean approach
 const root = createRoot(document.getElementById("root")!);
